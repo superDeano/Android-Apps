a7a7ec91c6699be1161c62ff442b0d5367694b8c
Dean <deancs1997@gmail.com>
2019-09-26 20:51:05 -0400

    Additional comments

--- app/src/main/java/com/example/grade/Profile.kt
+++ app/src/main/java/com/example/grade/Profile.kt
@@ -1,7 +1,9 @@
 package com.example.grade
+
 /**
  *
  * Profile Class which be used to pass data from the controller to the view
+ *The line below contains the properties of the class, the constructor
  *
  * */
-class Profile constructor(var name: String?, var age : String?, var id : String?)
+class Profile constructor(var name: String?, var age: String?, var id: String?)
