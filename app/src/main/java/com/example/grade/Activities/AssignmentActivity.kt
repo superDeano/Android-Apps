b9f7d1a96c515e64f9339050652708f19203ad0d
Dean <deancs1997@gmail.com>
2019-09-28 17:09:59 -0400

    Cleaned, Added Layout
    Preparing for addition of DB

---
+++ app/src/main/java/com/example/grade/Activities/AssignmentActivity.kt
@@ -0,0 +1,13 @@
+package com.example.grade.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.grade.R
+
+class AssignmentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_assignment)
+    }
+}
--- app/src/main/java/com/example/grade/Activities/MainActivity.kt
+++ app/src/main/java/com/example/grade/Activities/MainActivity.kt
@@ -6,39 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Button
-import android.widget.Toast
 import com.example.grade.R
-import com.example.grade.SharedPreferenceHelper
+
 
 class MainActivity : AppCompatActivity() {
 
 //    TODO: Clean
-//    private lateinit var showProfileButt: Button
-//    private lateinit var showMyGradesButt: Button
-    private lateinit var sharedPreference: SharedPreferenceHelper
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        sharedPreference = SharedPreferenceHelper(this)
-
-
-        //Getting the reference for the show my grades button
-//        showMyGradesButt = findViewById(R.id.seeGradesButt)
-//        showProfileButt = findViewById(R.id.goToProfileButt)
-
-        //Listener to know when the button is pressed onto
-//        showMyGradesButt.setOnClickListener {
-//            showRandomGrades()
-//
-//        }
-//        showProfileButt.setOnClickListener {
-//            showProfile()
-//        }
-
 
     }
 
@@ -57,22 +35,6 @@ class MainActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-//    override fun onStart() {
-//        super.onStart()
-
-////Checks if there is any information stored in SharedPreference
-//        if (sharedPreference.noInfoSaved()) {
-//            //If there is none, it loads the profile activity immediately
-//            showProfile()
-//            //Telling the user there is no information
-//            Toast.makeText(applicationContext, "No Profile Information Found!", Toast.LENGTH_LONG)
-//                .show()
-//
-//        } else if (sharedPreference.getProfile().name != null) {
-//            //There is information, thus displays the name in the button
-//            showProfileButt.text = (sharedPreference.getProfile().name)
-//        }
-//    }
 
     fun showRandomGrades() {
         // Intent to move to the next activity
