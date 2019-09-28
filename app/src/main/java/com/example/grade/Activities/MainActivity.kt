a882edc68acee2c96aba8f19697f6327dfbfdbc9
Dean <deancs1997@gmail.com>
2019-09-28 15:01:25 -0400

    making way for assignment 3
    Removing some stuffs here and there to make way for the next assignment requirements

--- app/src/main/java/com/example/grade/Activities/MainActivity.kt
+++ app/src/main/java/com/example/grade/Activities/MainActivity.kt
@@ -2,9 +2,10 @@
 package com.example.grade.Activities
 
 import android.content.Intent
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 import com.example.grade.R
@@ -12,8 +13,9 @@ import com.example.grade.SharedPreferenceHelper
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var showProfileButt: Button
-    private lateinit var showMyGradesButt: Button
+//    TODO: Clean
+//    private lateinit var showProfileButt: Button
+//    private lateinit var showMyGradesButt: Button
     private lateinit var sharedPreference: SharedPreferenceHelper
 
 
@@ -25,39 +27,54 @@ class MainActivity : AppCompatActivity() {
 
 
         //Getting the reference for the show my grades button
-        showMyGradesButt = findViewById(R.id.seeGradesButt)
-        showProfileButt = findViewById(R.id.goToProfileButt)
+//        showMyGradesButt = findViewById(R.id.seeGradesButt)
+//        showProfileButt = findViewById(R.id.goToProfileButt)
 
         //Listener to know when the button is pressed onto
-        showMyGradesButt.setOnClickListener {
-            showAllGrades()
-
-        }
-        showProfileButt.setOnClickListener {
-            showProfile()
-        }
+//        showMyGradesButt.setOnClickListener {
+//            showRandomGrades()
+//
+//        }
+//        showProfileButt.setOnClickListener {
+//            showProfile()
+//        }
 
 
     }
 
-    override fun onStart() {
-        super.onStart()
-
-//Checks if there is any information stored in SharedPreference
-        if (sharedPreference.noInfoSaved()) {
-            //If there is none, it loads the profile activity immediately
-            showProfile()
-            //Telling the user there is no information
-            Toast.makeText(applicationContext, "No Profile Information Found!", Toast.LENGTH_LONG)
-                .show()
-
-        } else if (sharedPreference.getProfile().name != null) {
-            //There is information, thus displays the name in the button
-            showProfileButt.text = (sharedPreference.getProfile().name)
-        }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu_profile, menu)
+        menuInflater.inflate(R.menu.main_menu_random_grades, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
-    fun showAllGrades() {
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.menuSeeProfile) {
+            showProfile()
+        } else if (item?.itemId == R.id.menuSeeRandomGrades) {
+            showRandomGrades()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+//    override fun onStart() {
+//        super.onStart()
+
+////Checks if there is any information stored in SharedPreference
+//        if (sharedPreference.noInfoSaved()) {
+//            //If there is none, it loads the profile activity immediately
+//            showProfile()
+//            //Telling the user there is no information
+//            Toast.makeText(applicationContext, "No Profile Information Found!", Toast.LENGTH_LONG)
+//                .show()
+//
+//        } else if (sharedPreference.getProfile().name != null) {
+//            //There is information, thus displays the name in the button
+//            showProfileButt.text = (sharedPreference.getProfile().name)
+//        }
+//    }
+
+    fun showRandomGrades() {
         // Intent to move to the next activity
         val moveToGradeActivity = Intent(this, gradeActivity::class.java)
         startActivity(moveToGradeActivity)
